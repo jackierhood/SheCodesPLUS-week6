bf10ff11c36e837b2094de6e2958f7a70f4469ae
Jackie Hood <jackierhood@gmail.com>
2020-12-04 14:12:32 -0500

    Current Location forecast

--- src/index.js
+++ src/index.js
@@ -122,6 +122,9 @@ function searchLocation(position) {
   let apiKey = "8617d97507bbab7e839be3563b098506";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=imperial`;
   axios.get(apiUrl).then(displayWeatherCondition);
+
+  apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function getCurrentLocation(event) {
